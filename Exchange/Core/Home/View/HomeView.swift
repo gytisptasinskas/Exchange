7b4ba068ac0b6805e49a21b29efc68760313dce5
Gytis Ptasinskas <ptasinskasgytis@gmail.com>
2024-02-05 11:20:41 +0200

    Initial HomeView Design

--- Exchange/Core/Home/View/HomeView.swift
+++ Exchange/Core/Home/View/HomeView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 10)  {
+                Button {
+                    print("Something should happen")
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .frame(width: 60, height: 60)
+                        .foregroundStyle(.white)
+                        .background(Color(uiColor: .systemBlue))
+                        .clipShape(Circle())
+                }
+            
+                Text("Add Currency Pair")
+                    .font(.headline)
+                    .foregroundStyle(Color(uiColor: .systemBlue))
+                
+                Text("Choose a currency pair to compare their \nlive rates")
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Color(uiColor: .systemGray))
+            }
+            .navigationTitle("Rates & converter")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
---
+++ Exchange/Core/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Exchange
+//
+//  Created by Gytis Ptasinskas on 05/02/2024.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
