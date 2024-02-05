cad958d7de57f56f5256e0d857d79c3c7b2960ca
Gytis Ptasinskas <ptasinskasgytis@gmail.com>
2024-02-05 10:46:58 +0200

    Initial commit

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Gytis Ptasinskas on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
